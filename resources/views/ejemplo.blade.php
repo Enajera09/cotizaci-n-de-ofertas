5b70b7a17a14b1b8ebf1087aa7f313c11fe2409c
Emerson-Najera <emersonnajera09@gmail.com>
2024-08-10 21:21:46 -0600

    first commit

---
+++ resources/views/ejemplo.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.index')
+@extends('layout.sidebar')
+@extends('layout.footer')
+
+@section('contenido')
+@include('layout.nav')
+
+<center><h1>Página de ejemplo</h1></center>
+
+@endsection
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="nav navbar navbar-expand-lg navbar-light iq-navbar">
+    <div class="container-fluid navbar-inner">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="mb-2 navbar-nav ms-auto align-items-center navbar-list mb-lg-0">
+
+                <li class="nav-item dropdown">
+                    <a class="py-0 nav-link d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="caption ms-3 d-none d-md-block ">
+                            <p class="mb-0 caption-sub-title">Jhon Doe</p>
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                        <li><a class="dropdown-item" href="../dashboard/auth/sign-in.html">Cerrar sesión</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
